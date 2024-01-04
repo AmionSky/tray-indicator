e03e66112a1307eeb1ebca21b21352ad4c0240d9
Csányi István <csanyistvan02@gmail.com>
2024-01-04 15:12:32 +0100

    Minor example changes

--- examples/simple/src/main.rs
+++ examples/simple/src/main.rs
@@ -3,15 +3,14 @@ use tray_indicator::{MenuItem, Tray};
 fn main() {
     println!("Simple tray-indicator example.");
 
-    let menu = vec![
-        MenuItem::label("Text"),
-        MenuItem::separator(),
-        MenuItem::button("Action", || println!("Simple action.")),
-        MenuItem::button("Exit", Tray::exit),
-    ];
-
     let mut tray = Tray::new(0x8189715b3bc5434da3ad10295dca7780, "Simple tray icon");
     tray.set_click(|| println!("The tray icon was clicked."));
-    tray.set_menu(menu);
+    tray.set_menu(vec![
+        MenuItem::label("A simple label"),
+        MenuItem::separator(),
+        MenuItem::button("Action button", || println!("Simple action.")),
+        MenuItem::button("Exit", Tray::exit),
+    ]);
+
     tray.display().expect("Failed to display tray icon");
 }
