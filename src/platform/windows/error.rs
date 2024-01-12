2e8f6c06c583dc75ab8bdd783d13cb4e67f364af
Csányi István <csanyistvan02@gmail.com>
2024-01-12 18:11:01 +0100

    Fix windows error message retrieval

--- src/platform/windows/error.rs
+++ src/platform/windows/error.rs
@@ -1,7 +1,7 @@
-use std::ptr::{null, null_mut};
-use windows_sys::Win32::Foundation::{GetLastError, LocalFree, WIN32_ERROR};
+use std::ptr::null;
+use windows_sys::Win32::Foundation::{GetLastError, WIN32_ERROR};
 use windows_sys::Win32::System::Diagnostics::Debug::{
-    FormatMessageW, FORMAT_MESSAGE_ALLOCATE_BUFFER, FORMAT_MESSAGE_FROM_SYSTEM,
+    FormatMessageW, FORMAT_MESSAGE_FROM_SYSTEM,
     FORMAT_MESSAGE_IGNORE_INSERTS,
 };
 
@@ -38,22 +38,15 @@ impl std::fmt::Display for WinError {
 impl std::error::Error for WinError {}
 
 unsafe fn get_error_message(code: WIN32_ERROR) -> String {
-    let buffer: *mut u16 = null_mut();
+    let mut buffer = [0u16; 4096];
     let length = FormatMessageW(
-        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+        FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
         null(),
         code,
         0,
-        buffer,
-        0,
+        buffer.as_mut_ptr(),
+        buffer.len() as u32,
         null(),
     );
-    let message = unicode_to_string(buffer, length);
-    LocalFree(buffer as _);
-    message
-}
-
-unsafe fn unicode_to_string(ptr: *const u16, len: u32) -> String {
-    let msg_native = std::slice::from_raw_parts(ptr, len as usize);
-    String::from_utf16_lossy(msg_native)
+    String::from_utf16_lossy(&buffer[..length as usize])
 }
